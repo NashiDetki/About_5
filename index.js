fc22a188afe13ccec7f56707353459119243a045
NashiDetki <93508687+NashiDetki@users.noreply.github.com>
2022-05-28 20:04:04 +0500

    com

---
+++ index.js
@@ -0,0 +1,11 @@
+const time =document.querySelector(".hedr-time");
+console.log(time);
+
+var counter = setInterval(function(){
+let date=new Date();
+ time.innerHTML = date.getHours()+ ":" + date.getMinutes();
+},1000);
+
+const dateobj = document.querySelector(".hedr-date");
+const date = new Date()
+dateobj.innerHTML = date.getDate() + "/"+ (Number (date.getMonth()) +1) + "/" + date.getFullYear();
